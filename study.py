d39aab79fb030065671e1f8e9d14bbb8a6f914c1
Alina Khomenko <ivanitskaya1998@gmail.com>
2026-02-09 16:00:46 +0200

    test.py

--- study.py
+++ study.py
@@ -15,10 +15,15 @@
 #for index, value in enumerate(some_list):
 #   print(index, value)
 
-def discount_price(price: int, discount: float) -> float:
-    def apply_discount():
-        nonlocal price
-        price = price * (1 - discount)
-
-    apply_discount()
-    return price
+# sentence_one = "Hello worlds"
+# sentence_two = "Hello " + "worlds"
+# # Відображення змінних
+# print(sentence_one, sentence_two, sep='\n')
+#
+# # Перевірка рівності і ідентичності
+# print(f'Are sentence equal? {sentence_one == sentence_two}')
+# print(f'Are sentence identical? {sentence_one is sentence_two}')
+#
+# # Перевірка місця збереження у памяті
+# print(f'Path of sentence in memory {id(sentence_one)}')
+# print(f'Path of sentence in memory {id(sentence_two)}')
