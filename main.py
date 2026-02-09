12467e94f01c3ddada9e8385841798a4151224d8
Alina Khomenko <ivanitskaya1998@gmail.com>
2026-02-09 16:00:46 +0200

    test.py

--- main.py
+++ main.py
@@ -9,10 +9,17 @@
 
 # Виведення (виведення даних)
 #print(вітання)
-def discount_price(price: int, discount: float) -> float:
-    def apply_discount():
-        nonlocal price
-        price = price * (1 - discount)
-
-    apply_discount()
-    return price
+# def discount_price(price: int, discount: float) -> float:
+#     def apply_discount():
+#         nonlocal price
+#         price = price * (1 - discount)
+#
+#     apply_discount()
+#     return price
+# def get_fullname(first_name, last_name, middle_name = ""):
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name
+# print(get_fullname("Alina", "Khomenko", "Marina"))
